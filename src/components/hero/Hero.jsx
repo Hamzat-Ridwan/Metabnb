98b3d25ee5295f79c010fcbd597cb3307b482a9e
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-11-11 06:05:22 +0100

    feat: Add search feature and bottom logos

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,15 +3,23 @@ import Image1 from '../../assets/image1.jpg'
 import Image2 from '../../assets/image2.png'
 import Image3 from '../../assets/image3.svg'
 import Image4 from '../../assets/image4.svg'
+import mbtoken from '../../assets/mbtoken.svg'
+import metamask from '../../assets/metamask.svg'
+import opensea from '../../assets/opensea.svg'
 
 import './Hero.css'
 
 const Hero = () => {
   return (
+    <>
     <main>
         <div className='texts'>
             <h1>Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span></h1>
             <p>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
+            <div className='search'>
+                <input type='search' placeHolder='Search for location' />
+                <button>Search</button>
+            </div>
         </div>
         <div className='images'>
             <div className='col-1'>
@@ -24,6 +32,12 @@ const Hero = () => {
             </div>
         </div>
     </main>
+    <div className='bottom-stipe'>
+        <img src={mbtoken} alt='com' />
+        <img src={metamask} alt='com' />
+        <img src={opensea} alt='com' />
+    </div>
+    </>
   )
 }
 
