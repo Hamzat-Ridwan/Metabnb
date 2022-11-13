a5a7d2a2e11957cadbaa78383e69618bbf11bfa0
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-11-13 09:47:32 +0100

    improve responsiveness of cards

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,4 +1,4 @@
-import card1 from '../assets/card1.jpg'
+import card1 from '../assets/card1.png'
 import card2 from '../assets/card2.jpg'
 import card3 from '../assets/card3.jpg'
 import card4 from '../assets/card4.jpg'
