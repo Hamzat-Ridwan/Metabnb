f05ce26a000cc83dec9a7ba5e5f04df06be2ed4f
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-11-14 01:22:17 +0100

    Add filters to places

--- src/pages/places/Places.jsx
+++ src/pages/places/Places.jsx
@@ -4,12 +4,29 @@ import Connect from '../../components/connect/Connect'
 import Footer from '../../components/footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
 import { PlacesCards } from '../../constants/CardsList'
+import { ReactComponent as Setting } from '../../assets/setting.svg'
 import './Places.css'
 
 const Places = () => {
   return (
     <div className='places'>
         <Navbar />
+        <div className='filters'>
+            <div className='texts'>
+                <p>Restaurant</p>
+                <p>Cottage</p>
+                <p>Castle</p>
+                <p>Fantastic city</p>
+                <p>Beach</p>
+                <p>Carbins</p>
+                <p>Off-grid</p>
+                <p>Farm</p>
+            </div>
+            <div className='setting'>
+                <p>Location</p>
+                <Setting />
+            </div>
+        </div>
         <div className='cards-con'>
             {
                 PlacesCards.map((img, index)=> (
